dc9eeda79400dcee06be9fd367db4940c36c725b
Juan Scabuzzo <Juanscabu@gmail.com>
2023-05-30 11:59:07 -0300

    added cors policies

--- EcommerceProject.Service.WebApi/Program.cs
+++ EcommerceProject.Service.WebApi/Program.cs
@@ -1,14 +1,17 @@
 using EcommerceProject.Service.WebApi;
 
 var builder = WebApplication.CreateBuilder(args);
+string myPolicy = "policyApiEcommerce";
 
-var configurationBuilder = new ConfigurationBuilder()
+var Configuration = new ConfigurationBuilder()
                 .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
                 .Build();
 
 builder.Services.RegisterServices();
-builder.Services.AddSingleton<IConfiguration>(configurationBuilder);
-
+builder.Services.AddSingleton<IConfiguration>(Configuration);
+builder.Services.AddCors(options => options.AddPolicy(myPolicy, builder => builder.WithOrigins(Configuration["Config:OriginCors"])
+                                                                                   .AllowAnyHeader()
+                                                                                   .AllowAnyMethod()));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -17,6 +20,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(myPolicy);
 
 app.UseAuthorization();
 
